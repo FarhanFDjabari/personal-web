3d700529948dd0ff149ba473086535dd8f385fda
FarhanFDjabari <ffadhilah8@gmail.com>
2021-08-30 00:45:37 +0800

    change website favicon

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Farhan Fadhilah Djabari\'s Website',
+      title: 'Farhan Fadhilah Djabari\'s Personal Website',
       theme: personalTheme,
       routes: {
         '/': (context) => LandingPage(),
