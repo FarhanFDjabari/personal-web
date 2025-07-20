4712ae2abab67d1a3162cc18f62f3d630646b479
FarhanFDjabari <djabari.dev@gmail.com>
2025-07-20 22:32:57 +0700

    Update medium link in constants.ts to reflect correct username

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const LINKS = {
     resume: "https://drive.google.com/file/d/17rDR9Bly4F9DfZVWqBX7rq5iWYZpOu8a/view",
     github: "https://github.com/farhanfdjabari",
     linkedin: "https://linkedin.com/in/farhanfdjabari",
-    medium: "https://medium.com/@farhanfdjabari"
+    medium: "https://medium.com/@djabaridev"
 } as const
